691741a955d49ea330725c36eb2bb561ef268133
Erkin Unlu <erkin.unlu@monitise.com>
2014-10-15 09:35:48 +0300

    problem 2

--- Ten.hs
+++ Ten.hs
@@ -1,8 +1,14 @@
 module Ten 
-( myLast
+( myLast,
+  myButLast
 ) where
 
 myLast :: [a] -> a
 myLast [] = error "can not work on empty list"
 myLast (x:[]) = x
 myLast (x:xs) = myLast xs
+
+myButLast :: [a] -> a
+myButLast [] = error "can not work on empty list"
+myButLast (x:y:[]) = x
+myButLast (x:xs) = myButLast xs
